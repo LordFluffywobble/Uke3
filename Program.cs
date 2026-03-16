fa93d616f700957b7a866c015a30755c3fe52afb
LordFluffywobble <38191273+LordFluffywobble@users.noreply.github.com>
2026-03-16 11:03:38 +0100

    Enkel while loop eksempel

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("*You wake up in a a small room and remember how you were caught while trying to gather some secret information*");
+        Console.WriteLine("");
+        Console.WriteLine("Welcome to my secret lair agent");
+        Console.WriteLine("Enjoy failing to get out of my ingenious trapps");
+        Console.WriteLine("MUAHAHAHAHHA!!!");
+        Console.WriteLine("");
+
+        while (true)
+        {
+            Console.WriteLine("@*There is a number keypad by the door in the otherwise barren room*\n*There is something scribbled on the side of the pad\n1...2...");
+            string keyP = Console.ReadLine()?.ToLower();
+            if (keyP == "3")
+            {
+                winCell();
+                return; 
+            }
+            else
+            {
+                loseGame();
+                return; 
+            }
+        }
+        
+    }
+    
+    static void winCell()
+    {
+        Console.WriteLine("Curses! You guessed it!!!");
+        Console.WriteLine("");
+        Console.WriteLine("*You exit the cell and step out in a small hallway that looks surprisingly homely*");
+        return;
+        
+    }
+    
+    
+    static void loseGame()
+    {
+        Console.WriteLine("Ahhh sweet death");
+        Console.WriteLine("");
+        return;
+    }
+}
+
+
+
+
+
+
