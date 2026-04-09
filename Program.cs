15da75c3baed5f371d962f711b4c4c4e2b1957fe
LordFluffywobble <38191273+LordFluffywobble@users.noreply.github.com>
2026-04-09 13:23:11 +0100

    Got help to fix a bug and introduced bool values

--- Program.cs
+++ Program.cs
@@ -4,57 +4,50 @@ class Program
     static void Main()
     {
         greetGame();
-        startGame();
-        winCell();
+    if( startGame() == false) { return; }
         hallWay();
         livingRoom();
     }
 
     static void loseGame()
     {
-        Console.WriteLine("");
-        Console.WriteLine("Ahhh sweet death");
-        Console.WriteLine("");
-        Environment.Exit(0);
+        Console.WriteLine("\nAhhh sweet death\n");
     }
 
     static void greetGame()
     {
-        Console.WriteLine("*There is a number keypad by the door in the otherwise barren room*\n*There is something scribbled on the side of the pad*\n1...2...");
-        Console.WriteLine("");
-        Console.WriteLine("*You wake up in a a small room and remember how you were caught while trying to gather some secret information*");
-        Console.WriteLine("");
+        Console.WriteLine("*There is a number keypad by the door in the otherwise barren room*\n*There is something scribbled on the side of the pad*\n1...2...\n");
+        Console.WriteLine("*You wake up in a a small room and remember how you were caught while trying to gather some secret information*\n");
         Console.WriteLine("Welcome to my secret lair agent");
         Console.WriteLine("Enjoy failing to get out of my ingenious traps");
-        Console.WriteLine("MUAHAHAHAHHA!!!");
-        Console.WriteLine("");
+        Console.WriteLine("MUAHAHAHAHHA!!!\n");
     }
 
-    static void startGame()
+    static bool startGame()
     {
-        Console.WriteLine("*There is a number keypad by the door in the otherwise barren room*\n*There is something scribbled on the side of the pad*\n1...2...");
-        string keyP = Console.ReadLine()?.ToLower();
-        switch (keyP)
+        while (true)
         {
-            case "3":
-                winCell();
-                break;
+            Console.Write("*There is a number keypad by the door in the otherwise barren room*\n*There is something scribbled on the side of the pad*\n1...2...");
+            string keyP = Console.ReadLine()?.ToLower();
+            switch (keyP)
+            {
+                case "3":
+                    winCell();
+                    return true;
 
-            case "666":
-                Console.WriteLine("");
-                Console.WriteLine("What do you think you're doing?");
-                Console.WriteLine("");
-                break;
+                case "666":
+                    Console.WriteLine("\nWhat do you think you're doing?\n");
+                    break;
 
-            default:
-                loseGame();
-                return;
+                default:
+                    loseGame();
+                    return false;
+            }
         }
     }
     static void winCell()
     {
-        Console.WriteLine("Curses! You guessed it!!!");
-        Console.WriteLine("");
+        Console.WriteLine("Curses! You guessed it!!!\n");
         Console.WriteLine("*You exit the cell and step out in a small hallway that looks surprisingly homely*");
     }
 
@@ -62,19 +55,16 @@ class Program
 
     static void hallWay()
     {
-        Console.WriteLine("");
-        Console.WriteLine("At the end of the hallway you see a stone statue with something written on a plaque underneath:");
+        //Console.WriteLine("");
+        Console.WriteLine("\nAt the end of the hallway you see a stone statue with something written on a plaque underneath:");
         Console.WriteLine("Speak the mighty words of Poe:");
-        Console.WriteLine("Never....?");
-        Console.WriteLine("");
+        Console.WriteLine("Nevermore....?");
 
         string keyP = Console.ReadLine()?.ToLower();
         switch (keyP)
         {
             case "nevermore":
-                Console.WriteLine("");
-                Console.WriteLine("You guessed correctly, please enter the livingroom\nThe statue moves aside and you enter the livingroom");
-                Console.WriteLine("");
+                Console.WriteLine("\nYou guessed correctly, please enter the livingroom\n\nThe statue moves aside and you enter the livingroom\n");
                 break;
 
             default:
